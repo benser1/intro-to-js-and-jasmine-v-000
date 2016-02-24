9e1e8663a999e4c9dd4aad0b6f2c68d50ec63a57
benser1 <briganenser28@gmail.com>
2016-02-24 13:35:20 -0700

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(name_of_icecream){
+  return "I love " + name_of_icecream;
+}
+
+function shouting(string){
+  return string.toUpperCase()
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
+
+function theTruth(){
+  return true;
+}
